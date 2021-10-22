8b6c9f761b9d84dbd4fae61f46ec351dbca441d5
Valkirja <daniil.kvach123@gmail.com>
2021-10-22 10:34:35 +0300

    Added task 1.2

---
+++ 1.2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "      |=====|" << endl;
+    cout << "      |     |" << endl;
+    cout << "      |     |" << endl;
+    cout << "      |     |" << endl;
+    cout << "     /       \\" << endl;
+    cout << "    /_________\\" << endl;
+    cout << "   |   Jack    |" << endl;
+    cout << "   |  Daniel's |" << endl;
+    cout << "   |           |" << endl;
+    cout << "   | Jennessee |" << endl;
+    cout << "   |   HONEY   |" << endl;
+    cout << "   |___________|" << endl;
+    cout << "   |           |" << endl;
+    cout << "   |___________|" << endl;
+    return 0;
+}
